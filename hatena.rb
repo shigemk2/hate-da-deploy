8244a377bd16601f02727a1cccc0ac1d56ccb325
shigemk2 <i.am.shige@gmail.com>
2013-10-27 20:54:55 +0900

    Fix element.value because encoding is strange

--- hatena.rb
+++ hatena.rb
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-require "rubygems"
-require "watir"
-require "watir-webdriver"
+# coding: utf-8
+require 'bundler'
+Bundler.require
+require 'yaml'
 
 # yamlファイル読み込み
-require 'yaml'
 yaml = YAML.load_file "hatena.yaml"
 
 # ログイン
-browser = Watir::Browser.new :chrome
+browser = Watir::Browser.new :ff
 browser.goto(yaml["login_url"])
 
 element = browser.text_field(:id, "login-name")
@@ -22,29 +21,30 @@ element.value = yaml["password"]
 element = browser.button(:class, "custom-button")
 element.flash
 element.click
+browser.wait
 
 # デザインページへアクセス
 browser.goto(yaml["detail_url"])
 
 # ヘッダーを編集
-header = File.open("hatena-header.html", "r:SHIFT_JIS")
+header = File.open("hatena-header.html", "r")
 element = browser.text_field(:id, "textarea1")
 element.flash
-element.value = header.read
+element.value = header.read.encode("utf-8")
 header.close
 
-# フッターを編集
-footer = File.open("hatena-footer.html", "r:SHIFT_JIS")
+# # フッターを編集
+footer = File.open("hatena-footer.html", "r")
 element = browser.text_field(:id, "textarea2")
 element.flash
-element.value = footer.read
+element.value = footer.read.encode("utf-8")
 footer.close
 
-# CSSを編集
-css = File.open("hatena.css", "r:SHIFT_JIS")
+# # CSSを編集
+css = File.open("hatena.css", "r")
 element = browser.text_field(:id, "textarea3")
 element.flash
-element.value = css.read
+element.value = css.read.encode("utf-8")
 css.close
 
 element = browser.button(:name, "save")
